c80fd92801d4c61cc76e92e477b88e5931169bcd
Nitto Joshi <nittojoshy1234@gmail.com>
2025-12-15 15:10:51 +0000

    make the logo small

--- src/components/SrishtiFestLogo.tsx
+++ src/components/SrishtiFestLogo.tsx
@@ -7,9 +7,9 @@ export const SrishtiFestLogo = ({ className }: { className?: string }) => (
       alt="Srishti 2.6 Logo"
       width={384}
       height={192}
-      sizes="(max-width: 768px) 16rem, 24rem"
+      sizes="(max-width: 768px) 12rem, 16rem"
       priority
-      className="w-64 md:w-96 h-auto"
+      className="w-48 md:w-64 h-auto"
     />
   </div>
 );
