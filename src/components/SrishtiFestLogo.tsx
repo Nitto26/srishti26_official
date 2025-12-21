59836e77a0deb9107414ac8205f1c9c7a8bcb8a7
Nitto Joshi <nittojoshy1234@gmail.com>
2025-12-21 10:59:45 +0000

    logo is in the public folder named "logo.png"

--- src/components/SrishtiFestLogo.tsx
+++ src/components/SrishtiFestLogo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const SrishtiFestLogo = ({ className }: { className?: string }) => (
   <div className={`relative ${className} w-full md:w-2/3 lg:w-1/2 h-24 md:h-32`}>
     <Image
-      src="/srishti-logo.svg"
+      src="/logo.png"
       alt="Srishti 2.6 Logo"
       fill
       className="object-contain"
