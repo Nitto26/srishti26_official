53bf0dd25718b55d3de14c9cd74161fd8c543e55
Nitto Joshi <nittojoshy1234@gmail.com>
2025-12-22 18:03:07 +0000

    no still not visble

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,13 @@ import './ImageGallery.css';
 import './Intro.css';
 import './Timeline.css';
 import { Toaster } from "@/components/ui/toaster";
-import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: 'SRISHTI 2.6',
   description: 'SRISHTI 2.6',
+  icons: {
+    icon: '/logo.png',
+  },
 };
 
 export default function RootLayout({
@@ -18,11 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <Head>
-        <link rel="stylesheet" href="/KSFE_files/main.min.css" />
-        <link rel="icon" href="/logo.png" />
-      </Head>
       <head>
+        <link rel="stylesheet" href="/KSFE_files/main.min.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
